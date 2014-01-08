43e5ad8b63355ebba03cb7c7fc7f1e03edf29ee3
seairy <seairy@gmail.com>
2014-01-08 09:51:49 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosemary::Application.config.secret_token = 'd7f8d9c752ba5fe41910e4ef048fae121218ac0a90e648b02174c65468938a0848a5d3fcafa4e75460eb2e76dd952f5456c08881c069fdc62b11ca90448b6d51'
