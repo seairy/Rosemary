7595ae56faeddd4e8eb063158b8c9a9d70970f26
seairy <seairy@gmail.com>
2014-01-20 16:58:47 +0800

    init commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,20 @@ module ApplicationHelper
       raw "<div class=\"notice\">#{flash[:notice]}</div>"
     end
   end
+  
+  def current_tag name
+    raw ' class="current"' if (if name.to_s == 'home' and controller_name == 'home'
+      true
+    elsif name.to_s == 'about_us' and controller_name == 'contents' and action_name == 'about_us'
+      true
+    elsif name.to_s == 'business_areas' and controller_name == 'contents' and action_name == 'business_areas'
+      true
+    elsif name.to_s == 'articles' and controller_name == 'articles'
+      true
+    elsif name.to_s == 'human_resources' and controller_name == 'contents' and action_name == 'human_resources'
+      true
+    elsif name.to_s == 'contact_us' and controller_name == 'contents' and action_name == 'contact_us'
+      true
+    end)
+  end
 end
