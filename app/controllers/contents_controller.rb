a7f1601e6268db6da8c96f11268aab8e704d158a
seairy <seairy@gmail.com>
2014-08-27 17:22:39 +0800

    bug fixed

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,7 +7,7 @@ class ContentsController < ApplicationController
   end
   
   def business_areas
-    @real_estate_projects = RealEstateProject.sorted.limit(4)
+    @real_estate_projects = RealEstateProject.sorted
   end
   
   def human_resources
